5f541559e693e438643b8a2c29eb25687fa9533b
Jake Esveld <thejakeesveld@gmail.com>
2019-06-12 11:01:24 -0700

    Initial commit

---
+++ src/test/kotlin/com/jakeesveld/zoos/ZoosApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.jakeesveld.zoos
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ZoosApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
