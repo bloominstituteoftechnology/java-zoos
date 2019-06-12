490624b9d14a17b524598fab93f788fc8b53f15f
Jake Esveld <thejakeesveld@gmail.com>
2019-06-12 13:14:37 -0700

    Add ZooService

--- src/main/java/com/jakeesveld/zoos/repo/ZooRepository.java
+++ src/main/java/com/jakeesveld/zoos/repo/ZooRepository.java
@@ -2,6 +2,7 @@ package com.jakeesveld.zoos.repo;
 
 import com.jakeesveld.zoos.model.Zoo;
 import com.jakeesveld.zoos.view.ZooDetails;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -11,4 +12,12 @@ public interface ZooRepository extends CrudRepository<Zoo, Long> {
 
     @Query(value = "SELECT z.zooname, a.animalid, t.phonenumber FROM zoo z INNER JOIN zooanimals a ON a.zooid=z.zooid INNER JOIN telephone t ON t.zooid=z.zooid GROUP BY z.zooid, z.zooname", nativeQuery = true)
     ArrayList<ZooDetails> getZooDetails();
+
+    @Modifying
+    @Query(value = "DELETE FROM zooanimals WHERE zooid=:zooid", nativeQuery = true)
+    void deleteZooAnimalsEntry(long zooid);
+
+    @Modifying
+    @Query(value = "DELETE FROM telephone WHERE zooid=:zooid", nativeQuery = true)
+    void deleteTelephonesForZoo(long zooid);
 }
---
+++ src/main/java/com/jakeesveld/zoos/service/ZooServiceImpl.java
@@ -0,0 +1,59 @@
+package com.jakeesveld.zoos.service;
+
+import com.jakeesveld.zoos.model.Animal;
+import com.jakeesveld.zoos.model.Telephone;
+import com.jakeesveld.zoos.model.Zoo;
+import com.jakeesveld.zoos.repo.ZooRepository;
+import com.jakeesveld.zoos.view.ZooDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+
+public class ZooServiceImpl implements ZooService {
+
+    @Autowired
+    private ZooRepository repo;
+
+    @Override
+    public ArrayList<ZooDetails> getZooDetails() {
+        return repo.getZooDetails();
+    }
+
+    @Override
+    public void updateZoo(long id, Zoo updatedZoo) {
+        Zoo currentZoo = repo.findById(id).orElseThrow(EntityNotFoundException::new);
+        if(updatedZoo.getZooname() != null){
+            currentZoo.setZooname(updatedZoo.getZooname());
+        }
+        if(updatedZoo.getTelephones().size() > 0){
+            for (Telephone telephone: updatedZoo.getTelephones()){
+                currentZoo.getTelephones().add(telephone);
+            }
+        }
+        if(updatedZoo.getAnimalList().size() > 0){
+            for (Animal animal: updatedZoo.getAnimalList()){
+                currentZoo.getAnimalList().add(animal);
+            }
+        }
+
+        repo.save(currentZoo);
+
+    }
+
+    @Override
+    public void addZoo(Zoo zoo) {
+        repo.save(zoo);
+    }
+
+    @Override
+    public void deleteZoo(long id) {
+        if (repo.findById(id).isPresent()) {
+            repo.deleteZooAnimalsEntry(id);
+            repo.deleteTelephonesForZoo(id);
+            repo.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Unable to find zoo with id: " + id);
+        }
+    }
+}
