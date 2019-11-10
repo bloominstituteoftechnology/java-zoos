d41c0031eeb55b96df6d6080922e1832591ee52e
John Mitchell <jrmmba@outlook.com>
2019-11-09 16:47:37 -0800

    Update data.sql

--- data.sql
+++ data.sql
@@ -10,38 +10,38 @@ FROM telephone;
 DELETE
 FROM zoo;
 
-INSERT INTO zoo (zooid, zooname)
-         VALUES (1, 'Gladys Porter Zoo'),
-                (2, 'Point Defiance Zoo'),
-                (3, 'San Diego Zoo'),
-                (4, 'San Antonio Zoo'),
-                (5, 'Smithsonian National Zoo');
+INSERT INTO zoo (zooid, zooname, created_by, created_date, last_modified_by, last_modified_date)
+         VALUES (1, 'Gladys Porter Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (2, 'Point Defiance Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (3, 'San Diego Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (4, 'San Antonio Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (5, 'Smithsonian National Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
 
-INSERT INTO telephone(phoneid, phonetype, phonenumber, zooid)
-         VALUES (1, 'main', '555-555-5555', 1),
-                (2, 'education', '555-555-1234', 1),
-                (3, 'membership', '555-555-4321', 1),
-                (4, 'main', '123-555-5555', 4),
-                (5, 'main', '555-123-5555', 3);
+INSERT INTO telephone(phoneid, phonetype, phonenumber, zooid, created_by, created_date, last_modified_by, last_modified_date)
+         VALUES (1, 'main', '555-555-5555', 1, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (2, 'education', '555-555-1234', 1, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (3, 'membership', '555-555-4321', 1, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (4, 'main', '123-555-5555', 4, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (5, 'main', '555-123-5555', 3, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
 
-INSERT INTO animal (animalid, animaltype)
-         VALUES (1, 'lion'),
-                (2, 'bear'),
-                (3, 'monkey'),
-                (4, 'penguin'),
-                (5, 'tiger'),
-                (6, 'bear'),
-                (7, 'turtle');
+INSERT INTO animal (animalid, animaltype, created_by, created_date, last_modified_by, last_modified_date)
+         VALUES (1, 'lion', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (2, 'bear', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (3, 'monkey', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (4, 'penguin', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (5, 'tiger', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (6, 'bear', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (7, 'turtle', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
 
-INSERT INTO zooanimals (zooid, animalid)
-         VALUES (1, 1),
-                (2, 2),
-                (1, 2),
-                (5, 7),
-                (5, 6),
-                (5, 5),
-                (5, 1),
-                (3, 1),
-                (3, 2);
+INSERT INTO zooanimals (zooid, animalid, created_by, created_date, last_modified_by, last_modified_date)
+         VALUES (1, 1, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (2, 2, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (1, 2, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (5, 7, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (5, 6, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (5, 5, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (5, 1, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (3, 1, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
+                (3, 2, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
 
 alter sequence hibernate_sequence restart with 10;
