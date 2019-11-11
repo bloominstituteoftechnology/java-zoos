3c25fc8eac1797d37e518771c277afc6d71fd6f3
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-11-11 14:35:57 -0700

    Update ZooServiceImpl.java

--- zoo/src/main/java/com/stepasha/zoo/services/ZooServiceImpl.java
+++ zoo/src/main/java/com/stepasha/zoo/services/ZooServiceImpl.java
@@ -1,41 +1,76 @@
 package com.stepasha.zoo.services;
 
+import com.stepasha.zoo.models.Telephone;
 import com.stepasha.zoo.models.Zoo;
+import com.stepasha.zoo.repos.ZooRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 
 @Service(value = "zooService")
 public class ZooServiceImpl implements ZooService{
 
+    @Autowired
+    private ZooRepository zoorepo;
+
+
     @Override
     public ArrayList<Zoo> findAllZoos() {
-        return null;
+       ArrayList<Zoo> list = new ArrayList<>();
+       zoorepo.findAll().iterator().forEachRemaining(list::add);
+       return list;
     }
 
     @Override
     public Zoo findZooById(long id) {
-        return null;
+        return zoorepo.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException(Long.toString(id)));
     }
 
     @Override
-    public Zoo findZooByName(String name) {
-        return null;
-    }
+    public Zoo findZooByName(String name) throws EntityNotFoundException{
+        Zoo zoo = zoorepo.findByZooname(name);
 
+        if (zoo == null)
+        {
+            throw new EntityNotFoundException("Zoo " + name + " not found!");
+        }
+        return zoo;
+    }
     @Override
-    public void deleteZoo(long id) {
-
+    public void deleteZoo(long id)     {
+        zoorepo.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Zoo id " + id + " not found!"));
+        zoorepo.deleteById(id);
     }
-
+@Transactional
     @Override
     public Zoo saveZoo(Zoo zoo) {
-        return null;
-    }
+        Zoo newZoo = new Zoo();
+        newZoo.setZooname(zoo.getZooname());
 
+        for(Telephone t: zoo.getTelephones()){
+            newZoo.getTelephones().add(new Telephone(t.getPhonetype(), t.getPhonenumber(), newZoo));
+        }
+        return zoorepo.save(newZoo);
+    }
+@Transactional
     @Override
     public Zoo updateZoo(Zoo zoo, long id) {
-        return null;
+    Zoo currentZoo = findZooById(id);
+    if (zoo.getZooname() != null) {
+        currentZoo.setZooname(zoo.getZooname());
+    }
+    if (zoo.getTelephones() != null) {
+        for (Telephone t : zoo.getTelephones()) {
+            currentZoo.getTelephones().add(new Telephone(t.getPhonetype(), t.getPhonenumber(), currentZoo));
+        }
+    }
+    return zoorepo.save(currentZoo);
+
     }
 }
 
