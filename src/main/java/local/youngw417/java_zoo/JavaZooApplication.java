1c043a866c89d70c12668b0f0f867c466efd00b4
Young Won <youngwon@Youngs-MacBook-Pro.local>
2020-08-04 21:33:57 -0700

    2nd

---
+++ src/main/java/local/youngw417/java_zoo/JavaZooApplication.java
@@ -0,0 +1,13 @@
+package local.youngw417.java_zoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaZooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaZooApplication.class, args);
+    }
+
+}
