d4e65a7ef4ff4c4066c299b48278eeca08320486
aldenho52 <69052933+aldenho52@users.noreply.github.com>
2020-12-10 12:15:04 -0700

    building models

---
+++ javazoos/src/main/java/com/lambdaschool/javazoos/models/Telephone.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.javazoos.models;
+
+public class Telephone
+{
+}
---
+++ javazoos/src/main/java/com/lambdaschool/javazoos/models/Zoo.java
@@ -0,0 +1,64 @@
+package com.lambdaschool.javazoos.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "zoos")
+public class Zoo extends Auditable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long zooid;
+
+    @Column(nullable = false, unique = true)
+    private String zooname;
+
+    @OneToMany(mappedBy = "zoo",
+    cascade = CascadeType.ALL,
+    orphanRemoval = true)
+    @JsonIgnoreProperties(value = "zoo", allowSetters = true)
+    private List<Telephone> telephones = new ArrayList<>();
+
+    @OneToMany(mappedBy = "zoo",
+    cascade = CascadeType.ALL,
+    orphanRemoval = true)
+    @JsonIgnoreProperties(value = "user", allowSetters = true)
+    private Set<ZooAnimals> animals = new HashSet<>();
+
+    public Zoo()
+    {
+    }
+
+    public Zoo(
+        String zooname)
+    {
+        this.zooname = zooname;
+    }
+
+    public long getZooid()
+    {
+        return zooid;
+    }
+
+    public void setZooid(long zooid)
+    {
+        this.zooid = zooid;
+    }
+
+    public String getZooname()
+    {
+        return zooname;
+    }
+
+    public void setZooname(String zooname)
+    {
+        this.zooname = zooname;
+    }
+}
