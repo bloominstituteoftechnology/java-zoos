413a6474094d514d9c21a88dd869afdd4edcb817
Tarah <tarah.agbokhana@gmail.com>
2021-04-15 18:43:55 -0400

    Model CleanUp

--- src/main/java/com/lambdaschool/zoos/models/Animal.java
+++ src/main/java/com/lambdaschool/zoos/models/Animal.java
@@ -21,12 +21,6 @@ public class Animal extends Auditable{
   cascade = CascadeType.ALL,
   orphanRemoval = true)
   @JsonIgnoreProperties(value = "animal", allowSetters = true)
-  private List<Telephone> telephones = new ArrayList<>();
-
-  @OneToMany(mappedBy = "animal",
-  cascade = CascadeType.ALL,
-  orphanRemoval = true)
-  @JsonIgnoreProperties(value = "animal", allowSetters = true)
   private Set<ZooAnimals> animals = new HashSet<>();
 
   public Animal() {
@@ -51,14 +45,6 @@ public class Animal extends Auditable{
 
   public void setAnimaltype(String animaltype) {
     this.animaltype = animaltype.toLowerCase();
-  }
-
-  public List<Telephone> getTelephones() {
-    return telephones;
-  }
-
-  public void setTelephones(List<Telephone> telephones) {
-    this.telephones = telephones;
   }
 
   public Set<ZooAnimals> getAnimals() {
--- src/main/java/com/lambdaschool/zoos/models/Zoo.java
+++ src/main/java/com/lambdaschool/zoos/models/Zoo.java
@@ -1,6 +1,9 @@
 package com.lambdaschool.zoos.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
+import java.util.*;
 
 @Entity
 @Table(name = "zoos")
@@ -10,7 +13,18 @@ public class Zoo {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long zooid;
 
+  @OneToMany(mappedBy = "zoo",
+      cascade = CascadeType.ALL,
+      orphanRemoval = true)
+  @JsonIgnoreProperties(value = "zoo", allowSetters = true)
+  private List<Telephone> telephones = new ArrayList<>();
   String zooname;
+
+  @OneToMany(mappedBy = "zoo",
+      cascade = CascadeType.ALL,
+      orphanRemoval = true)
+  @JsonIgnoreProperties(value = "zoo", allowSetters = true)
+  private Set<ZooAnimals> animals = new HashSet<>();
 
   public Zoo() {
   }
@@ -34,5 +48,21 @@ public class Zoo {
 
   public void setZooname(String zooname) {
     this.zooname = zooname.toLowerCase();
+  }
+
+  public List<Telephone> getTelephones() {
+    return telephones;
+  }
+
+  public void setTelephones(List<Telephone> telephones) {
+    this.telephones = telephones;
+  }
+
+  public Set<ZooAnimals> getAnimals() {
+    return animals;
+  }
+
+  public void setAnimals(Set<ZooAnimals> animals) {
+    this.animals = animals;
   }
 }
